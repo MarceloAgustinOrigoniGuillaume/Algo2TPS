c64f5e607ed3456ee36886b0fbc195fd60b71274
MarceloAgustinOrigoniGuillaume <111713074+MarceloAgustinOrigoniGuillaume@users.noreply.github.com>
2022-10-08 18:43:52 -0300

    primer implementacion
    se implemento las funciones basicas, todas excepto el iterador externo y las funciones de hasheo como tal

--- hash/hashCuckoo.go
+++ hash/hashCuckoo.go
@@ -18,8 +18,16 @@ type elementoHash[K comparable, V any]{
 	valor V
 }
 
+func crearElementoHash[K comparable, V any](clave K, valor V) elementoHash[K,V]{
+	elemento:= new(elementoHash[K,V])
+
+	elemento.clave = clave
+	elemento.valor = valor
+}
+
 type hashCuckoo[K comparable, V any] struct{
-	elementos []elementoHash
+	elementos []*elementoHash
+	cantidad int
 }
 
 
@@ -30,40 +38,87 @@ func CrearHashCuckoo[K comparable, V any]() Hash[K,V]{
 
 }
 
-func (hash *hashCuckoo) 
+func buscarPosicionCuckoo(elementos []elementoHash, clave K) int{
+	secuencia := []func() int {funcionHashing1,funcionHashing2,funcionHashing3}
+	for _,funcionHash := range secuencia{
+		valor := funcionHash(clave) % len(elementos)
+		if(elementos[valor] == nil || elementos[valor].clave == clave){
+			return valor 
+		}
+	}
+
+	return -1
+}
+func posicionarElementoCuckoo(elementos []elementoHash, elemento elementoHash) bool{
+	posicion := buscarPosicionCuckoo(elementos,elemento.clave)
+	if(posicion == -1){
+		return false
+	}
+
+	elementos[posicion] = elemento
+
+	return true
+}
 
 func (hash *hashCuckoo) redimensionar(nuevoLargo int){
 	elementosNew := make([]elementoHash, nuevoLargo)
+	hash.Iterar(func (clave K, valor V) {
+		posicionarElementoCuckoo(elementosNew,CrearHashCuckoo(clave,valor))
+	})
 
-	copy(elementosNew, hash.elementos)
 	hash.elementos = elementosNew
 }
 
 
 
 func (hash *hashCuckoo) Guardar(clave K, valor V){
-	
+	nuevo:= crearElementoHash(clave,valor)
+	if(!posicionarElementoCuckoo(hash.elementos,nuevo)){
+		hash.redimensionar(2 * len(hash.elementos))
+		
+		if(!posicionarElementoCuckoo(hash.elementos,nuevo)){
+			return
+		}
+	}
+
+	hash.cantidad++
 }
 
 
 func (hash *hashCuckoo) Pertenece(clave K) bool{
-	
+	i:= buscarPosicionCuckoo(hash.elementos,clave)
+	return i != -1 && hash.elementos[i] != nil
 }
 
 func (hash *hashCuckoo) Obtener(clave K) V{
+	i:= buscarPosicionCuckoo(hash.elementos,clave)
+	if(i != -1 && hash.elementos[i] != nil){
+		return hash.elementos[i].valor
+	}
+	return nil
 	
 }
 
 func (hash *hashCuckoo) Borrar(clave K) V{
+	i:= buscarPosicionCuckoo(hash.elementos,clave)
 	
+	if(i != -1 && hash.elementos[i] != nil){
+		elem := hash.elementos[i].valor
+		hash.elementos[i] = nil
+		hash.cantidad--
+		return elem
+	}
 }
 
 func (hash *hashCuckoo) Cantidad() int{
-	
+	return hash.cantidad
 }
 	
 func (hash *hashCuckoo) Iterar(visitar func(clave K, dato V) bool){
-	
+	i:= 0
+	for (i<len(hash.elementos) && hash.elementos[i] == nil && visitar(hash.elementos[i].clave, hash.elementos[i].valor)){
+		i++
+	}	
 }
 
 func (hash *hashCuckoo) Iterador() IterDiccionario[K,V]{
