4fa8042b9511154d4b78cab429888ca902590d6d
Marcelo <marce.origoni@gmail.com>
2022-11-26 11:31:36 -0300

    Comment change

--- tp2/utilities/utilities.go
+++ tp2/utilities/utilities.go
@@ -4,7 +4,6 @@ import "os"
 import "bufio"
 import "strings"
 
-// ../../pruebaTp2/users_test.txt
 func LeerArchivo(url string, haceAlgo func(string) bool) error {
 	archivo, error := os.Open(url)
 	if error != nil {
