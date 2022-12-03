69bd29b0663bff40c35d8339e72d60783b6d28fb
Marcelo <marce.origoni@gmail.com>
2022-12-03 16:00:50 -0300

    otro intento con el mk

--- tp3/entrega.mk
+++ tp3/entrega.mk
@@ -1,3 +1,6 @@
-	go build -o ./vamosmoshi;
+vamosmoshi:
+	go build -o ./vamosmoshi ./vamosmoshi.go;
+test_gen:
 	go build -o ./test_gen testing/test_gen.go;
+test_file:
 	go build -o ./test_file testing/test_file.go;
